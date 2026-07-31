72d495080725fe13f88d7d7250aa8c8216fd0934
Pacto7107 <architectseoha5222@gmail.com>
2026-07-31 12:34:04 +0900

    ai못ㅎ람

--- js/nav.js
+++ js/nav.js
@@ -30,7 +30,6 @@ async function renderNav(activePage) {
     { href: "contests.html", label: "대회", key: "contests" },
     { href: "ranking.html", label: "랭킹", key: "ranking" },
     { href: "show_tier_list.html", label: "티어 목록", key: "tiers" }, // 티어 메뉴 추가
-    { href: "battle.html", label: "배틀", key: "battle" }, 
     { href: "shop.html", label: "상점", key: "shop" },
     { href: "devnotes.html", label: "개발자노트", key: "devnotes" },
   ];
